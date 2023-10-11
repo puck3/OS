ff4f027908d6c30fd54e6ea8463397fdb9215e26
puck3 <shipilovk04@gmail.com>
2023-10-11 03:16:06 +0300

    fix multithreading

--- lw2/main.cpp
+++ lw2/main.cpp
@@ -2,126 +2,150 @@
 #include <vector>
 #include <pthread.h>
 #include <math.h>
+#include <unordered_map>
 #include <chrono>
 
 using namespace std;
 
-pthread_mutex_t mutex;
+pthread_mutex_t set_id_lock; // name?
 
-typedef struct args {
-    int id;
-    int threadCount;
-    vector<vector<double>>* result;
-    vector<vector<double>>* matrix;
-    int sizeX, sizeY;
-    vector<vector<double>>* filter;
-    int filterSizeX, filterSizeY;
-} args_t;
+pthread_mutex_t completed_lock;
+pthread_cond_t completed_all;
 
-void scan(vector<vector<double>>& matrix, int sizeX, int sizeY) {
-    for (int i = 0; i < sizeX; i++) {
-        for (int j = 0; j < sizeY; j++) {
-            cin >> matrix[i][j];
+vector<vector<double>> result;
+vector<vector<double>> matrix;
+vector<vector<double>> filter;
+int thread_count, id = 0, completed_count = 0;
+int size_x, size_y, filter_size_x, filter_size_y, k;
+
+
+void scan(vector<vector<double>>& m, int x, int y) {
+    for (int i = 0; i < x; i++) {
+        for (int j = 0; j < y; j++) {
+            cin >> m[i][j];
         }
     }
 }
 
-void print(const vector<vector<double>>& matrix, int sizeX, int sizeY) {
-    for (int i = 0; i < sizeX; i++) {
-        for (int j = 0; j < sizeY; j++) {
+void print(const vector<vector<double>>& m, int x, int y) {
+    for (int i = 0; i < x; i++) {
+        for (int j = 0; j < y; j++) {
             cout.precision(2);
-            cout << fixed << matrix[i][j] << " ";
+            cout << fixed << m[i][j] << " ";
         }
         cout << endl;
     }
 }
 
-void* applyFilter(void* args) {
-    args_t* arg = (args_t*) args;
+void apply_filter(int i, int j) {
+    int half_x = filter_size_x / 2, half_y = filter_size_y / 2;
+    double sum = 0.0;
 
-    int id = arg->id;
-    const int& threadCount = arg->threadCount;
-    const vector<vector<double>>& matrix = *arg->matrix;
-    const int& sizeX = arg->sizeX;
-    const int& sizeY = arg->sizeY;
-
-    const vector<vector<double>>& filter = *arg->filter;
-    const int& filterSizeX = arg->filterSizeX;
-    const int& filterSizeY = arg->filterSizeY;
-
-    for (int i = (sizeX / threadCount) * (id); (i < sizeX) && (i < ceil((double) sizeX / threadCount) * (id + 1)); i++) {
-        for (int j = 0; j < sizeY; j++) {
-            double sum = 0.0;
-            int halfX = filterSizeX / 2, halfY = filterSizeY / 2;
-
-            for (int x = 0; x <= halfX; x++) {
-                for (int y = 0; y <= halfY; y++) {
-                    // check indexes for out of range
-                    sum += matrix[(i - halfX + x >= 0) ? (i - halfX + x) : 0][(j - halfY + y >= 0) ? (j - halfY + y) : 0] * filter[x][y]; // left top corner
-                    if (y != halfY) {
-                        sum += matrix[(i - halfX + x >= 0) ? (i - halfX + x) : 0][(j + halfY - y < sizeY) ? (j + halfY - y) : (sizeY - 1)] * filter[x][filterSizeY - y - 1]; // right top
-                    }
-                    if (x != halfX) {
-                        sum += matrix[(i + halfX - x < sizeX) ? (i + halfX - x) : (sizeX - 1)][(j - halfY + y >= 0) ? (j - halfY + y) : 0] * filter[filterSizeX - x - 1][y]; // left bot
-                    }
-                    if (x != halfX && y != halfY) {
-                        sum += matrix[(i + halfX - x < sizeX) ? (i + halfX - x) : (sizeX - 1)][(j + halfY - y < sizeY) ? (j + halfY - y) : (sizeY - 1)] * filter[filterSizeX - x - 1][filterSizeY - y - 1]; // right bot
-                    }
-                }
+    for (int x = 0; x <= half_x; x++) {
+        for (int y = 0; y <= half_y; y++) {
+            // check indexes for out of range
+            sum += matrix[(i - half_x + x >= 0) ? (i - half_x + x) : 0][(j - half_y + y >= 0) ? (j - half_y + y) : 0] * filter[x][y]; // left top corner
+            if (y != half_y) {
+                sum += matrix[(i - half_x + x >= 0) ? (i - half_x + x) : 0][(j + half_y - y < size_y) ? (j + half_y - y) : (size_y - 1)] * filter[x][filter_size_y - y - 1]; // right top
             }
-            arg->result->at(i)[j] = sum / (filterSizeX * filterSizeY);
+            if (x != half_x) {
+                sum += matrix[(i + half_x - x < size_x) ? (i + half_x - x) : (size_x - 1)][(j - half_y + y >= 0) ? (j - half_y + y) : 0] * filter[filter_size_x - x - 1][y]; // left bot
+            }
+            if (x != half_x && y != half_y) {
+                sum += matrix[(i + half_x - x < size_x) ? (i + half_x - x) : (size_x - 1)][(j + half_y - y < size_y) ? (j + half_y - y) : (size_y - 1)] * filter[filter_size_x - x - 1][filter_size_y - y - 1]; // right bot
+            }
         }
+    }
+    result[i][j] = sum / (filter_size_x * filter_size_y);
+}
+
+void apply_to_raws(int start, int finish) {
+    for (int i = start; (i < size_x) && (i < finish); i++) {
+        for (int j = 0; j < size_y; j++) {
+            apply_filter(i, j);
+        }
+    }
+}
+
+void apply_to_cols(int start, int finish) {
+    for (int i = 0; i < size_x; i++) {
+        for (int j = start; (j < size_y) && (j < finish); j++) {
+            apply_filter(i, j);
+        }
+    }
+}
+
+
+void* filter_thread(void* arg) {
+    void(*apply_func)(int, int);
+    apply_func = (size_x >= size_y) ? apply_to_raws : *apply_to_cols;
+    pthread_mutex_lock(&set_id_lock);
+    int tid = id++;
+    id %= thread_count;
+    pthread_mutex_unlock(&set_id_lock);
+
+    int start = (size_x / thread_count) * (tid);
+    int finish = ceil((double) size_x / thread_count) * (tid + 1);
+
+    for (int _ = 0; _ < k; ++_) {
+        apply_func(start, finish);
+        pthread_mutex_lock(&completed_lock);
+        if (++completed_count == thread_count) {
+            matrix = result;
+            completed_count = 0;
+            pthread_cond_broadcast(&completed_all);
+        } else {
+            pthread_cond_wait(&completed_all, &completed_lock);
+        }
+        pthread_mutex_unlock(&completed_lock);
     }
     pthread_exit(0);
 }
 
 int main(int argc, char* argv[]) {
-    auto begin = std::chrono::steady_clock::now();
     freopen("in.txt", "r", stdin);
 
-    args_t args;
-    args.threadCount = stoi(argv[1]);
-
-    pthread_t tid[args.threadCount];
     // cout << "Enter matrix size: ";
-    cin >> args.sizeX >> args.sizeY;
-
-    vector<vector<double>> matrix(args.sizeX, vector<double>(args.sizeY));
+    cin >> size_x >> size_y;
+    matrix = result = vector<vector<double>>(size_x, vector<double>(size_y));
     // cout << "Enter matrix:" << endl;
-    scan(matrix, args.sizeX, args.sizeY);
-    args.matrix = &matrix;
+    scan(matrix, size_x, size_y);
 
     // cout << "Enter filter window size (odd number of rows and cols): ";
-    cin >> args.filterSizeX >> args.filterSizeY;
-    if (!(args.filterSizeX & 1) || !(args.filterSizeY & 1)) {
+    cin >> filter_size_x >> filter_size_y;
+    if (!(filter_size_x & 1) || !(filter_size_y & 1)) {
         throw runtime_error("Even number of rows or cols in filter window");
     }
-
-    vector<vector<double>> filter(args.filterSizeX, vector<double>(args.filterSizeY));
+    filter = vector<vector<double>>(filter_size_x, vector<double>(filter_size_y));
     // cout << "Enter convolution matrix:" << endl;
-    scan(filter, args.filterSizeX, args.filterSizeY);
-    args.filter = &filter;
+    scan(filter, filter_size_x, filter_size_y);
 
-    int k;
     // cout << "K = ";
     cin >> k;
-    vector<vector<double>> result(args.sizeX, vector<double>(args.sizeY, 0));
-    args.result = &result;
 
-    for (int i = 0; i < k; ++i) {
-        for (int i = 0; i < args.threadCount; i++) {
-            args.id = i;
-            pthread_create(&tid[i], NULL, applyFilter, (void*) &args);
-        }
-        for (int i = 0; i < args.threadCount; i++) {
-            pthread_join(tid[i], NULL);
-        }
-        args.matrix = &result;
+    thread_count = stoi(argv[1]);
+
+    if (size_x > size_y && thread_count > size_x) {
+        thread_count = size_x;
+    } else if (size_x < size_y && thread_count > size_y) {
+        thread_count = size_y;
     }
-    cout.precision(6);
-    cout << "Result:" << endl;
-    print(*args.matrix, args.sizeX, args.sizeY);
+
+    pthread_t tid[thread_count];
+
+    auto begin = std::chrono::steady_clock::now();
+    for (int i = 0; i < thread_count; i++) {
+        pthread_create(&tid[i], nullptr, filter_thread, nullptr);
+    }
+    for (int i = 0; i < thread_count; i++) {
+        pthread_join(tid[i], nullptr);
+    }
     auto end = std::chrono::steady_clock::now();
+
+    cout.precision(3);
+
+    cout << "Result:" << endl;
+    print(matrix, size_x, size_y);
     auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
     std::cout << "The time: " << elapsed_ms.count() << " ms\n";
 
